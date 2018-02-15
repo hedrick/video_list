fe38cbcdfc3ea6e37aec3183f8fafe9ff0a9c657
Scott Hedrick <me@scottshedrick.com>
2018-02-15 14:31:47 -0600

    added JSX to search_bar.js and added to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAvxQW4me3Jg31Ws-yMU4vALEO00LV_3u8'
+
 const App = () => {
-  return <div>Hi</div>
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
