30534e4dd054d24fef7b5a989950805f6e51158f
Scott Hedrick <me@scottshedrick.com>
2018-02-15 16:23:00 -0600

    now capable of selecting a video from videolist

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const videoItems = videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />
+    return (
+      <VideoListItem
+        onVideoSelect={onVideoSelect} 
+        key={video.etag} 
+        video={video} 
+      />
+    )
   });
 
   return (
