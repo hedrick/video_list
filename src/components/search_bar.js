5bcd2ac4be59937ef75ee2df95c05e3ed6b89902
Scott Hedrick <me@scottshedrick.com>
2018-02-15 16:26:08 -0600

    added basic styling to components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,11 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input 
           value = {this.state.term}
           onChange={event => this.setState({ term: event.target.value })} 
-        />
+      />
       </div>
     );
   }
